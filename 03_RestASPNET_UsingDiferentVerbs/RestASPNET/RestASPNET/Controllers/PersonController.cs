fa46adada7e6ff0a2cda341a8a34b6379dce1e83
Robert Madureira <robeet73@gmail.com>
2022-03-02 23:23:15 -0300

    using verbs

---
+++ 03_RestASPNET_UsingDiferentVerbs/RestASPNET/RestASPNET/Controllers/PersonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+           
+            return BadRequest("Invalid Input");
+        }
+
+
+        
+    }
+}
