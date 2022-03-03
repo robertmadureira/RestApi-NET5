77f8e1555b71c42cba50d16688a3dc90c5f6fd1c
Robert Madureira <robeet73@gmail.com>
2022-03-03 00:24:33 -0300

    using verbs

--- 03_RestASPNET_UsingDiferentVerbs/RestASPNET/RestASPNET/Startup.cs
+++ 03_RestASPNET_UsingDiferentVerbs/RestASPNET/RestASPNET/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestASPNET.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,9 @@ namespace RestASPNET
         {
 
             services.AddControllers();
+
+            //Dependency Injection //Injeção de dependência
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
