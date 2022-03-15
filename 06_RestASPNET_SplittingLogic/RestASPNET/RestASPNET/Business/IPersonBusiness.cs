75a74a8b5f1e0a6a70c1e86fbacbfba5957624cc
Robert Madureira <robeet73@gmail.com>
2022-03-14 22:05:08 -0300

    SplittingLogic

---
+++ 06_RestASPNET_SplittingLogic/RestASPNET/RestASPNET/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestASPNET.Model;
+using System.Collections.Generic;
+
+namespace RestASPNET.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ 06_RestASPNET_SplittingLogic/RestASPNET/RestASPNET/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestASPNET.Model.Context;
+using RestASPNET.Business.Implementations;
+using RestASPNET.Business;
+using RestASPNET.Repository;
+using RestASPNET.Repository.Implementations;
+
+namespace RestASPNET
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            //Versionamento API
+            services.AddApiVersioning();
+
+            //Dependency Injection //Injeção de dependência
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
